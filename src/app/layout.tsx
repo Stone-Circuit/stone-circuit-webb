89aad232fc9dd3a070f51c4074de948c7b516495
stonecircuit <hellostonecircuit@gmail.com>
2026-07-18 10:57:27 +0530

    Add Microsoft Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/context/ThemeContext";
 import ShellLayout from "@/components/navigation/ShellLayout";
 import "@/styles/globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
 
 export const viewport: Viewport = {
   width: "device-width",
@@ -129,6 +130,14 @@ export default function RootLayout({
           <ShellLayout>{children}</ShellLayout>
         </ThemeProvider>
         <GoogleAnalytics gaId="G-5K8X8CJYD5" />
+        {`
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xo814h9wgh");
+</script>`}
       </body>
     </html>
   );
